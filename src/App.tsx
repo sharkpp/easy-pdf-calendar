720bfaef4cc678ec0333b56c52276ef55b8b555a
さめたすたす <sharkpp@users.noreply.github.com>
2025-01-06 00:19:55 +0900

    PDFの生成処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,16 @@
-import { useCallback, useRef, useState } from 'react';
+import { useState } from 'react';
 import { css } from '@emotion/react';
-import tmpl from '../designs/simple-light-pict/main.svg?raw';
-import { jsPDF } from 'jspdf';
-import 'svg2pdf.js';
-import { useFont } from './hooks/jspdf-usefont';
-import { PageSize } from './utils/jspdf-pagesize';
 import DesignList from './components/DesignList';
 import CalanderDesignPreview from './components/CalandarDesignPreview';
 
-const MS24H = 24 * 60 * 60 * 1000;
-
-function getSVGLenByMM(n: SVGAnimatedLength) {
-  const tmp = n.baseVal;
-  tmp.convertToSpecifiedUnits(SVGLength.SVG_LENGTHTYPE_MM);
-  return tmp.valueInSpecifiedUnits;
-}
-
+// 表示するUIの種類
 const UI = {
-  SelectDesign: 0,
-  PreviewCalendarMonth: 1,
+  SelectDesign: 0, // デザインの選択画面
+  PreviewCalendarMonth: 1, // カレンダーのプレビュー画面
 };
 
 function App()
 {
-  const refSvgHideContainer: React.MutableRefObject<null | HTMLIFrameElement> = useRef(null);
-  const pdfRef: React.MutableRefObject<null | HTMLIFrameElement> = useRef(null);
-  const notosans = useFont("Noto Sans Gothic", "/assets/fonts/NotoSansJP-Medium.ttf")
-  const handleMakePDF = useCallback(() => {
-    async function makePdf() {
-      const year = 2025;
-      const doc = new jsPDF({
-        orientation: 'l',
-        format: PageSize.B6JIS
-      });
-      const svgContainer = document.createElement("div");
-      svgContainer.innerHTML = tmpl;
-      const svgCalendar = svgContainer.firstElementChild;
-      await notosans.install(doc);
-      if (svgCalendar) {
-        doc.setFont(notosans.name);
-        const monthes = new Array(12).fill(0).map((_, i: number) => i + 1);
-        for await (const month of monthes) {
-          if (1 < month) {
-            doc.addPage(PageSize.B6JIS, 'l');
-          }
-          const svgCalendar_ = svgCalendar.cloneNode(true) as SVGElement;
-          if (refSvgHideContainer.current) {
-            refSvgHideContainer.current.replaceChildren(svgCalendar_);
-          }
-          //const monthElm = svgCalendar_.querySelector('*[id="month"],*[inkscape\\:label="month"]');
-          // 月は *[inkscape\\:label="month"] 要素の表示を変える
-          const monthElm: SVGElement | null = svgCalendar_.querySelector(`*[id="m-${month}"],*[inkscape\\:label="m-${month}"]`);
-          if (monthElm) {
-            monthElm.style.display = "";
-          }
-          const firstDateOfMonth = new Date(year, month-1, 1); // 今月の最初の日
-          const firstDayOfWeek = firstDateOfMonth.getDay(); // 今月最初の日の曜日(日曜:0 - 土曜:6)
-          const lastDateOfMonth = new Date(new Date(year, month-1+1, 1).getTime() - MS24H).getDate(); // 今月の最後の日
-          const lastDateOfPrevMonth = new Date(firstDateOfMonth.getTime() - MS24H).getDate(); // 先月の最後の日
-
-          let dateBox = new Array(7 * 6).fill(0); // 0:空欄 1〜:今月の日 〜-1:先月or来月の日
-          // 今月の日を設定
-          for (let i = 0; i < lastDateOfMonth; ++i) {
-            dateBox[i + firstDayOfWeek] = i + 1;
-          }
-          // 先月の日を設定
-          for (let i = 0; i < 7; ++i) {
-            if (0 <= firstDayOfWeek - 1 - i) {
-              dateBox[firstDayOfWeek - 1 - i] = -(lastDateOfPrevMonth - i);
-            }
-          }
-          // 来月の日を設定
-          for (let i = 0; i < 14; ++i) {
-            if (firstDayOfWeek + lastDateOfMonth + i < dateBox.length) {
-              dateBox[firstDayOfWeek + lastDateOfMonth + i] = -(i + 1);
-            }
-          }
-
-          console.log({firstDateOfMonth,firstDayOfWeek,lastDateOfMonth,lastDateOfPrevMonth,dateBox});
-
-          dateBox.forEach((date, dateIndex) => {
-            const dateBoxElm = svgCalendar_.querySelector(`*[id^="day-${dateIndex}"],*[inkscape\\:label^="day-${dateIndex}"]`);
-            console.log({date,dateIndex,dateBoxElm,x:getSVGLenByMM((dateBoxElm as SVGRectElement).x),y:getSVGLenByMM((dateBoxElm as SVGRectElement).y)});
-            doc.text(
-              ""+Math.abs(date),
-              (dateBoxElm as SVGRectElement).x.baseVal.value + (dateBoxElm as SVGRectElement).width.baseVal.value / 2,
-              (dateBoxElm as SVGRectElement).y.baseVal.value + (dateBoxElm as SVGRectElement).height.baseVal.value / 2,
-              {
-                align: "center",
-                baseline: "middle",
-              });
-          });
-
-          await doc.svg(svgCalendar_);
-        }
-
-
-        if (pdfRef.current) {
-          pdfRef.current.src = doc.output('datauristring');
-        }
-      }
-    }
-    makePdf();
-  }, []);
-
   const [ design, setDesign ] = useState("");
   const [ year, setYear ] = useState(2025);
   const [ month, setMonth ] = useState(1);
@@ -126,23 +33,10 @@ function App()
           design={design}
           year={year}
           month={month}
-          onChangeDesign={(designName) => {
-            setDesign(designName);
-          }}
-          onChangeMonth={(month) => {
-            setMonth(month);
-          }}
+          onChangeDesign={(designName) => setDesign(designName)}
+          onChangeMonth={(month) => setMonth(month)}
         />}
       </div>
-      {/*
-      <div ref={refSvgHideContainer} css={css`width: 0px;height: 200px;`}>
-      </div>
-      <div css={css`width: 100%;height: 100%;`}>
-        <Button
-          onClick={handleMakePDF}
-        >make pdf</Button>
-        <iframe ref={pdfRef} css={css`width: 100%;height: calc(100% - 40px);`} />
-      </div>*/}
     </>
   )
 }
--- src/components/PopupPrintPreview.tsx
+++ src/components/PopupPrintPreview.tsx
@@ -1,6 +1,6 @@
 // 印刷プレビュー画面
 
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { css } from '@emotion/react';
 import {
   DialogBody,
@@ -10,7 +10,12 @@ import {
   DialogRoot as Dialog,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Button } from "@chakra-ui/react"
 import { OpenChangeDetails } from '@zag-js/dialog';
+import { jsPDF } from 'jspdf';
+import 'svg2pdf.js';
+import { PageSize } from '@/utils/jspdf-pagesize';
+import { useFont } from '@/hooks/jspdf-usefont';
 import { useCalendar } from '@/store/calendar';
 
 // 画像切り取りポップアップのプロパティの型
@@ -21,6 +26,16 @@ type PopupImageCropperProps = {
   onOpenChange: (open: boolean) => void;
 }
 
+const MS24H = 24 * 60 * 60 * 1000;
+
+const MONTH_LIST = new Array(12).fill(0).map((_, i: number) => i + 1);
+
+function getSVGLenByMM(n: SVGAnimatedLength) {
+  const tmp = n.baseVal;
+  tmp.convertToSpecifiedUnits(SVGLength.SVG_LENGTHTYPE_MM);
+  return tmp.valueInSpecifiedUnits;
+}
+
 function PopupPrintPreview({
   design, year,
   open, onOpenChange,
@@ -28,18 +43,55 @@ function PopupPrintPreview({
 
   const { getCalendar, setCalendar } = useCalendar();
 
+  const notosans = useFont("Noto Sans Gothic", "/assets/fonts/NotoSansJP-Medium.ttf")
+  
   //const colorMode = useColorMode().colorMode as 'dark' | 'light' | undefined;
   //console.log(colorMode)
 
   const refContener = useMemo(() => {
     return (div: HTMLDivElement | null) => {
-      console.log({div})
       if (div) {
         const calendars = new Array(12).fill(0).map((_, i) => (
           getCalendar(`${design}:${year}:${i+1}`)
         ));
         console.log({calendars})
         div.replaceChildren.apply(div, calendars as Node[]);
+      }
+    };
+  }, []);
+
+  const refPdf = useMemo(() => {
+    return (iframe: HTMLIFrameElement | null) => {
+      if (iframe) {
+        // PDF構築
+        async function makePdf() {
+          // PDFドキュメントを準備
+          const doc = new jsPDF({
+            orientation: 'l',
+            format: PageSize.B6JIS
+          });
+          // フォントをインストール
+          await notosans.install(doc);
+          doc.setFont(notosans.name);
+          // カレンダーを取得
+          let calendarOfMonth;
+          for await (const month of MONTH_LIST) {
+            if (1 < month) { // 最初の月のみページを追加しない
+              doc.addPage(PageSize.B6JIS, 'l');
+            }
+            // カレンダーのSVGを取得
+            calendarOfMonth = getCalendar(`${design}:${year}:${month}`);
+            // SVGをPDFに変換
+            if (calendarOfMonth) {
+              await doc.svg(calendarOfMonth as Element);
+            }
+          }
+  
+          if (iframe) {
+            iframe.src = doc.output('datauristring');
+          }
+        }
+        makePdf();
       }
     };
   }, []);
@@ -65,7 +117,23 @@ function PopupPrintPreview({
           <DialogTitle>カレンダーの印刷</DialogTitle>
           <DialogCloseTrigger />
         </DialogHeader>
-        <DialogBody>
+        <DialogBody
+          css={css`
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            & > iframe {
+              flex-grow: 1;
+              width: 100%;
+              height: 100%;
+            }
+          `}
+        >
+
+          <iframe
+           ref={refPdf}
+          />
+          
           <div
             ref={refContener}
             css={css`
@@ -76,6 +144,10 @@ function PopupPrintPreview({
                 }
               `}
           />
+
+          <Button alignSelf="stretch">
+            完了
+          </Button>
         </DialogBody>
       </DialogContent>
     </Dialog>
