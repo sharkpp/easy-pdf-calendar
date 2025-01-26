7d98a19231f7a17f9067b8944bd566ba4e61c5f5
さめたすたす <sharkpp@users.noreply.github.com>
2025-01-27 00:58:50 +0900

    印刷レイアウトの設定を追加

--- src/utils/jspdf-pagesize.ts
+++ src/utils/jspdf-pagesize.ts
@@ -4,7 +4,8 @@ export type PageSizeType = [number, number];
 
 // ページサイズ(単位はmm)
 export const PageSize: { [key:  string]: PageSizeType } = {
-  // A判
+
+  // A判 比率：1.41379310345
   A0:  [ 841, 1189 ],
   A1:  [ 594,  841 ],
   A2:  [ 420,  594 ],
@@ -16,7 +17,8 @@ export const PageSize: { [key:  string]: PageSizeType } = {
   A8:  [  52,   74 ],
   A9:  [  52,   37 ],
   A10: [  37,   26 ],
-  // JIS B判
+
+  // JIS B判 比率：1.41359223301
   B0JIS: [ 1030, 1456 ],
   B1JIS: [  728, 1030 ],
   B2JIS: [  515,  728 ],
@@ -28,5 +30,15 @@ export const PageSize: { [key:  string]: PageSizeType } = {
   B8JIS: [   64,   91 ],
   B9JIS: [   45,   64 ],
   B10JIS:[   32,   45 ],
+
+  // L判 比率：1.42696629213
+  '2L':  [  127,  178 ], // 2L判
+  L:     [   89,  127 ], // L判
+
+  // はがき 比率：1.48
+  POSTCARD:[  100,  148 ],
+
+  // 名刺
+  BUSINESS_CARD_4:[ 55, 91 ], // 名刺４号サイズ
 
 } as const;
