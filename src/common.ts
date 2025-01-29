ab54dea88f53868140e0f38b764f66b5859a45e4
さめたすたす <sharkpp@users.noreply.github.com>
2025-01-29 13:12:43 +0900

    生成時に付与されるURLを変更

--- src/common.ts
+++ src/common.ts
@@ -1,7 +1,7 @@
 // 共通の定数など
 
 // カレンダーデザインの基準パス
-export const CALENDAR_DESIGNS_BASE_PATH = '/assets/designs';
+export const CALENDAR_DESIGNS_BASE_PATH = import.meta.env.BASE_URL + 'assets/designs';
 
 // 印刷レイアウトの基準パス
-export const PRINT_LAYOUT_BASE_PATH = '/assets/layouts';
+export const PRINT_LAYOUT_BASE_PATH = import.meta.env.BASE_URL + 'assets/layouts';
